504c0606aed5e60499ad7da57589baa24891f0d6
Ivan <ivan.zderadicka@gmail.com>
2023-08-20 13:16:16 +0200

    Add post to Author

--- catalog/src/main/java/eu/zderadicka/mbs3/rest/AuthorResource.java
+++ catalog/src/main/java/eu/zderadicka/mbs3/rest/AuthorResource.java
@@ -2,14 +2,19 @@ package eu.zderadicka.mbs3.rest;
 
 import java.util.List;
 
+import org.jboss.resteasy.reactive.RestResponse;
+
 import eu.zderadicka.mbs3.orm.Author;
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("api/v1/authors")
 public class AuthorResource extends BaseResource {
@@ -26,5 +31,11 @@ public class AuthorResource extends BaseResource {
     @Path("/{id}")
     public Uni<Author> getById(@PathParam("id") Long id) {
         return Author.findByIdOrThrow(id);
+    }
+
+    @POST
+    public Uni<RestResponse<Author>> create(Author author) {
+        return Panache.withTransaction(author::persist).replaceWith(RestResponse.status(Status.CREATED, author));
+
     }
 }
